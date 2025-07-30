d1799975dfbdd188bc61047493b963d31a759618
2420030552lahari <2420030552@klh.edu.in>
2025-07-30 11:24:27 +0530

    Add files via upload
    lab1
    1)addnumbers
    2)temperature
    3)volumeofsphere

---
+++ LAB1/src/temperature.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class temperature {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int Fahrenheit;
+		    System.out.println("Enter the temperature: ");
+		    Scanner sc =new Scanner(System.in);
+		    Fahrenheit=sc.nextInt();
+		    int Celsius=(Fahrenheit-32)*5/9;
+		    System.out.println("The temperature in Celsius= "+Celsius);
+	}
+
+}
